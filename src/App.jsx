2963cc31389f744c55bcbac178a9cafdedb9039a
itssShiva <iitts.shiva@gmail.com>
2025-11-07 11:27:57 +0530

    Routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Routes,Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar.jsx";
+import Body from "./components/Body.jsx";
+import Login from "./components/Login.jsx";
+import Profile from "./components/Profile.jsx";
 function App() {
   return (
     <>
  
-    <Navbar/>
-      <h1 className="text-3xl underline">Namaste</h1>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path='/' element={<Body/>}>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
