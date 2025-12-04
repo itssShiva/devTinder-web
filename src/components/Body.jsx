ffd883c1a114711f7c39e3a2aaaa58f12ed1f8c7
itssShiva <iitts.shiva@gmail.com>
2025-12-04 17:40:50 +0530

    Restricted Apis for unauthorized access

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Footer from './Footer'
+import axios from 'axios'
+import {BASE_URL} from '../utils/Constant'
+import { useDispatch } from 'react-redux'
+import { addUser } from '../utils/userSlice'
 
 const Body = () => {
+
+  const navigate=useNavigate();
+  const dispatch=useDispatch();
+  const fetchUser=async()=>{
+    try {
+      const res=await axios.get(BASE_URL+'/profile/view',{
+      withCredentials:true,
+    })
+    dispatch(addUser(res.data));
+    } catch (error) {
+        if(error.status===401){
+          navigate('/login');
+        }
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchUser();
+  },[])
   return (
     <div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,45 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const user=useSelector((store)=>store.user)
   console.log(user)
   return (
-    <div>
+    
+ <div>
       <div className="navbar bg-base-300 shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl"> 👩‍💻DevTinder</a>
+          {user?(<Link to={'/'} className="btn btn-ghost text-xl"> 👩‍💻DevTinder</Link>):(<Link to={'/login'} className="btn btn-ghost text-xl"> 👩‍💻DevTinder</Link>)}
         </div>
-        <div className="flex gap-2">
+        {user&&<div className="flex gap-2">
           <div className="dropdown dropdown-end mx-8">
             <div
               tabIndex={0}
               role="button"
               className="btn btn-ghost btn-circle avatar"
             >
-            {user&&  <div className="w-10 rounded-full">
+            {user?(<div className="w-10 rounded-full">
                 <img
                   alt="User Photo"
                   src={user.photoUrl}
                 />
-              </div>}
+              </div>):(<div className="w-10 rounded-full">
+                <img
+                  alt="User Photo"
+                  src={"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/195px-Heart_coraz%C3%B3n.svg.png?20110326231420"}
+                />
+              </div>)}
             </div>
             <ul
               tabIndex="-1"
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to={'/profile'} className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
                 <a>Settings</a>
@@ -42,7 +49,7 @@ const Navbar = () => {
               </li>
             </ul>
           </div>
-        </div>
+        </div>}
       </div>
     </div>
   );
