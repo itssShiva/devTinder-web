faa7b899f26c9bf7c110c388928355e3a45111a4
itssShiva <iitts.shiva@gmail.com>
2025-12-05 20:05:22 +0530

    created feed api for feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { BASE_URL } from '../utils/Constant';
+import { addFeed } from '../utils/feedSlice';
 
 const Feed = () => {
+
+  const dispatch=useDispatch();
+  const feed=useSelector((store)=>store.feed);
+  const getFeed=async()=>{
+    try {
+      if(feed) return;
+      const res= await axios.get(BASE_URL+"/feed",{withCredentials:true});
+      console.log(res.data);
+      dispatch(addFeed(res?.data))
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    getFeed();
+  },[])
+  
   return (
     <div>Feed</div>
   )
