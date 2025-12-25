798de26625b5b7e558e8361096a739a5fd8a9e69
Shiva Gupta <iitts.shiva@gmail.com>
2025-12-26 00:35:57 +0530

    razorpay dialog opened

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BASE_URL } from '../utils/Constant'
+import axios from 'axios'
 
 
 const Premium = () => {
@@ -9,8 +10,32 @@ const Premium = () => {
       membershipType:type
     },{withCredentials:true}
   )
+  
+  console.log(res.data);
+  console.log(res?.data?.order?.amount);
 
-  console.log(res);
+
+    const options = {
+        key: res?.data?.key, // Replace with your Razorpay key_id
+        amount: res?.data?.order?.amount, // Amount is in currency subunits.
+        currency: 'INR',
+        name: 'Dev Tinder',
+        description: 'Become a Premium Developer',
+        order_id: res?.data?.order?.id, // This is the order_id created in the backend
+        prefill: {
+          name: res?.data?.order?.notes?.firstName+" "+res?.data?.order?.notes?.lastName,
+          email: res?.data?.order?.notes?.emailId,
+          contact: '9999999999'
+        },
+        theme: {
+          color: '#1e1e1e'
+        },
+      };
+
+
+   const rzp = new window.Razorpay(options);
+      rzp.open();
+
   }
   return (
     <div className='flex justify-center my-43 gap-10'>
@@ -18,7 +43,7 @@ const Premium = () => {
   <div className="card-body">
     <div className="flex justify-between">
       <h2 className="text-3xl font-bold">Silver Membership</h2>
-      <span className="text-xl">$2/mo</span>
+      <span className="text-xl">₨.2/mo</span>
     </div>
     <ul className="mt-6 flex flex-col gap-2 text-xs">
       <li>
@@ -50,7 +75,7 @@ const Premium = () => {
     <span className="badge badge-xs badge-warning">Most Popular</span>
     <div className="flex justify-between">
       <h2 className="text-3xl font-bold">Gold Membership</h2>
-      <span className="text-xl">$10/mo</span>
+      <span className="text-xl">₨.10/mo</span>
     </div>
     <ul className="mt-6 flex flex-col gap-2 text-xs">
       <li>
