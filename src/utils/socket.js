f4fb6e2940208f23585503e84201f95d69aea12e
Shiva Gupta <iitts.shiva@gmail.com>
2026-01-09 15:43:20 +0530

    production ready

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client'
 import {BASE_URL} from './Constant'
 
 export const createSocketConnection=()=>{
-    if(location.hostname='localhost'){
+    if(location.hostname=='localhost'){
         return io(BASE_URL);
     }
     else{
